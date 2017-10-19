26fb22b46ab0832b66167d8114bda9f8054cc706
Joshua Vandaële <joshua@vandaele.software>
2017-10-19 22:27:10 +0200

    woops

--- luaUtils.lua
+++ luaUtils.lua
@@ -128,7 +128,7 @@ function sleep(s)
 end
 
 function os.clear()
-  if not os.execute("clear") or not os.execute("cls") then -- if not linux/windows
+  if not os.execute("clear") and not os.execute("cls") then -- if not linux/windows
     for i = 1,25 do -- print many \n's
       print("\n\n")
     end
